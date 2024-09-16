19dc9a68350fa63995176f57a38b62dd6896f02b
pshanawazkhan <pshanawazkhan@gmail.com>
2024-09-16 11:01:09 +0530

    changes commit1

---
+++ src/main/java/com/example/demo/controller/MainService.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MainService {
+
+	
+	
+	
+	
+	public String getData() {
+		
+		
+		return "Data from main Service class------------";
+	}
+	
+	
+	
+	
+	
+}
